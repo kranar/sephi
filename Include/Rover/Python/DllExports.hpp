c3fa02580aba3f6d3b2a90aaaf912a96aa39cf9e
Kamal Mansouri <kamal@eidolonsystems.com>
2024-02-09 22:45:55 -0500

    Rover compiles all starter code.

--- Include/Rover/Python/DllExports.hpp
+++ Include/Rover/Python/DllExports.hpp
@@ -1,22 +1,22 @@
-#ifndef ASPEN_PYTHON_DLL_EXPORTS
-#define ASPEN_PYTHON_DLL_EXPORTS
+#ifndef ROVER_PYTHON_DLL_EXPORTS
+#define ROVER_PYTHON_DLL_EXPORTS
 
 #ifdef _MSC_VER
-  #define ASPEN_EXTERN
-  #if defined(ASPEN_BUILD_DLL)
-    #define ASPEN_EXPORT_DLL __declspec(dllexport)
-  #elif defined(ASPEN_USE_DLL)
-    #define ASPEN_EXPORT_DLL __declspec(dllimport)
+  #define ROVER_EXTERN
+  #if defined(ROVER_BUILD_DLL)
+    #define ROVER_EXPORT_DLL __declspec(dllexport)
+  #elif defined(ROVER_USE_DLL)
+    #define ROVER_EXPORT_DLL __declspec(dllimport)
   #else
-    #define ASPEN_EXPORT_DLL
+    #define ROVER_EXPORT_DLL
   #endif
 #else
-  #if defined(ASPEN_BUILD_DLL) || defined(ASPEN_USE_DLL)
-    #define ASPEN_EXTERN extern
-    #define ASPEN_EXPORT_DLL __attribute__((visibility ("default")))
+  #if defined(ROVER_BUILD_DLL) || defined(ROVER_USE_DLL)
+    #define ROVER_EXTERN extern
+    #define ROVER_EXPORT_DLL __attribute__((visibility ("default")))
   #else
-    #define ASPEN_EXTERN
-    #define ASPEN_EXPORT_DLL
+    #define ROVER_EXTERN
+    #define ROVER_EXPORT_DLL
   #endif
 #endif
 
